88ceb7920caea35909a2c8378fcd3b8e03375fbd
quakkels <quakkels@gmail.com>
2020-01-21 19:44:31 -0600

    Adding gunicorn to dependencies.

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='tiaamd',
-    version='0.1.2',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'flask',
-				'beautifulsoup4',
-				'feedparser',
-				'requests',
-        'aiohttp',
-        'asyncio'
-    ],
-		python_requires='>=3.7'
+	name='tiaamd',
+	version='0.1.2',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'flask',
+		'beautifulsoup4',
+		'feedparser',
+		'requests',
+		'aiohttp',
+		'asyncio',
+		'gunicorn'
+	],
+	python_requires='>=3.7'
 )
