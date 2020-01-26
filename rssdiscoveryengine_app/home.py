3a05236ce4a119630c54ec49cc6a604dcdfea2f8
quakkels <quakkels@gmail.com>
2020-01-26 11:18:28 -0600

    Update UI and styles.

--- rssdiscoveryengine_app/home.py
+++ rssdiscoveryengine_app/home.py
@@ -12,19 +12,16 @@ from rssfinderasync import rfasync
 bp = Blueprint('home', __name__,
 	template_folder='templates')
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/', methods=(['GET']))
 def index():
-	blog_url = ''
 	results = None
-	if request.method == 'POST':
-		blog_url = request.form['blog_url']
-		if blog_url is None:
-			pass
-		elif is_blog_url_valid(blog_url):
-			# rss_feed_url = rssfinder.find_rss_url(blog_url)
-			# results = rssfinder.find_links(rss_feed_url)
-			results = rfasync.initiate_finder(blog_url)
-			print("finished getting results")
+	blog_url = request.args.get('blog_url')
+	print(f'blog_url: {blog_url}')
+	if blog_url is None:
+		blog_url = ''
+	elif is_blog_url_valid(blog_url):
+		results = rfasync.initiate_finder(blog_url)
+		print("finished getting results")
 
 	return render_template('home.html', blog_url = blog_url,
 		results = results)
